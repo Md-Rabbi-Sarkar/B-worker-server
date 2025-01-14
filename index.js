961561f227b462a4c355c6bd681acc7bada0a573
Md-Rabbi-Sarkar <noreply@github.com>
2025-01-14 18:26:16 +0600

    create post api for user set in database

--- index.js
+++ index.js
@@ -7,6 +7,66 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uu4gd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const userCollection = client.db('BworkerDB').collection('users')
+    app.post('/users',async(req,res)=>{
+        const user = req.body
+        const query = {email: user.email}
+        const existingUser = await userCollection.findOne(query)
+        const worker = user?.role ==='worker'
+        const buyer = user?.role ==='buyer'
+        const options = { upsert: true };
+        if(existingUser){
+            return res.send({message: 'user already exists',insertedId: null})
+        }
+        let coin =0
+        if(worker){
+            coin=10 
+           
+        }
+        if(buyer){
+            coin=50
+             
+        }
+        const newUser ={
+            name:user.name,
+            email:user.email,
+            role:user.role,
+            coin,
+
+        }
+
+const result = await userCollection.insertOne(newUser)
+        
+        res.send(result)
+    })
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/',(req,res)=>{
     res.send('server are work')
 })
