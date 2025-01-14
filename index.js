8294b377cbcf69334402944ebcf0df8cecfe7135
Md-Rabbi-Sarkar <noreply@github.com>
2025-01-14 19:22:14 +0600

    create middlewair as verifytoken,admin ,worker, buyer

--- index.js
+++ index.js
@@ -2,74 +2,109 @@ require('dotenv').config()
 const express = require('express')
 const app = express();
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
 
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uu4gd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    const userCollection = client.db('BworkerDB').collection('users')
-    app.post('/users',async(req,res)=>{
-        const user = req.body
-        const query = {email: user.email}
-        const existingUser = await userCollection.findOne(query)
-        const worker = user?.role ==='worker'
-        const buyer = user?.role ==='buyer'
-        const options = { upsert: true };
-        if(existingUser){
-            return res.send({message: 'user already exists',insertedId: null})
-        }
-        let coin =0
-        if(worker){
-            coin=10 
-           
-        }
-        if(buyer){
-            coin=50
-             
-        }
-        const newUser ={
-            name:user.name,
-            email:user.email,
-            role:user.role,
-            coin,
-
-        }
-
-const result = await userCollection.insertOne(newUser)
+    try {
+        const userCollection = client.db('BworkerDB').collection('users')
         
-        res.send(result)
-    })
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        const verifyToken = (req,res,next)=>{
+            if(!req.headers.authorization){
+                return res.status(401).send({message: ' Unauthorized access'})
+            }
+            const token = req.headers.authorization.split(' ')[1]
+            jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+                if(err){
+                    return res.status(401).send({message: ' Unauthorized access'})
+                }
+                req.decoded = decoded;
+                next()
+            })
+        }
+        const verifyAdmin = async (req,res)=>{
+            const email = req.decoded.email
+            const query = {email: email}
+            const user = await userCollection.findOne(query)
+            const isAdmin = user?.role === 'admin'
+            if(!isAdmin){
+                return res.status(403).send({message: 'Forbidden access'})
+            }
+            next()
+        }
+        const verifyBuyer = async (req,res)=>{
+            const email = req.decoded.email
+            const query = {email: email}
+            const user = await userCollection.findOne(query)
+            const isBuyer = user?.role === 'buyer'
+            if(!isBuyer){
+                return res.status(403).send({message: 'Forbidden access'})
+            }
+            next()
+        }
+        const veryfyWorker = async(req,res)=>{
+            const email = req.decoded.email
+            const query = {email: email}
+            const user = await userCollection.findOne(query)
+            const isWorker = user?.role === 'worker'
+            if(!isWorker){
+                return res.status(403).send({message: 'Forbidden access'})
+            }
+            next()
+        }
+        app.post('/jwt',async(req,res)=>{
+            const user =req.body
+            const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'24h'})
+            res.send({token})
+        })       
+        app.post('/users', async (req, res) => {
+            const user = req.body
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query)
+            const worker = user?.role === 'worker'
+            const buyer = user?.role === 'buyer'
+            if (existingUser) {
+                return res.send({ message: 'user already exists', insertedId: null })
+            }
+            let coin = 0
+            if (worker) {
+                coin = 10
+            }
+            if (buyer) {
+                coin = 50
+            }
+            const newUser = {
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                coin,
+            }
+            const result = await userCollection.insertOne(newUser)
+            res.send(result)
+        })
+        
+    } finally {
+    }
 }
 run().catch(console.dir);
 
-
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('server are work')
 })
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server is running on port ${port}`)
 })
